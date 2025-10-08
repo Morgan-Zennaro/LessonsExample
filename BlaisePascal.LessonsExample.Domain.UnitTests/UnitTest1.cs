4de972cf8ecdc0519d092e55bcb25dfd3e22c44d
w11 <w11@PC-4623>
2025-10-08 10:26:04 +0200

    Aggiungere i file di progetto.

---
+++ BlaisePascal.LessonsExample.Domain.UnitTests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace BlaisePascal.LessonsExample.Domain.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Enemy enemy = new Enemy();
+            enemy.SetHealth(82);
+        }
+    }
+}
---
+++ BlaisePascal.LessonsExample.Domain/Class1.cs
@@ -0,0 +1,50 @@
+﻿namespace BlaisePascal.LessonsExample.Domain
+{
+    public class Enemy
+    {
+        private int _health;
+        private string _name;
+        public Enemy(string name, int health)
+        {
+            SetHealth(health);
+            SetName(name);
+        }
+        public Enemy(int health)
+        {
+            SetHealth(health);
+            SetName("Enemy");
+        }
+        public Enemy(string name)
+        {
+            SetHealth(100);
+            SetName(name);
+        }
+        public Enemy()
+        {
+            SetHealth(100);
+            SetName("Enemy");
+        }
+        public void SetName (string newName)
+        {
+            if (!string.IsNullOrWhiteSpace(newName))
+            {
+                _name = newName;
+            }
+        }
+        public void SetHealth (int newHealth)
+        {
+            if (int.IsPositive(newHealth) && newHealth <= 100)
+            {
+                _health = newHealth;
+            }
+        }
+        public string GetName()
+        {
+            return _name;
+        }
+        public int GetHealth()
+        {
+            return _health;
+        }
+    }
+}
