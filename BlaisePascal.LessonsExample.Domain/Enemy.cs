a190da1ca4c5630411826b36746b62eca3db6871
Morgan-Zennaro <morgan.zennaro.stud@ispascalcomandini.it>
2025-10-10 09:47:03 +0200

    modifiche

--- BlaisePascal.LessonsExample.Domain/Enemy.cs
+++ BlaisePascal.LessonsExample.Domain/Enemy.cs
@@ -2,26 +2,43 @@
 {
     public class Enemy
     {
-        public bool _isAlive;
+        public int Health { get; private set; }
+        public string Name { get; private set; }
+        public bool IsAlive { get; private set; };
         public string _description;
-        private int _health;
-        private string _name;
         public Enemy(string name, int health)
         {
-            SetHealth(health);
             SetName(name);
+            SetHealth(health);
+            IsAlive = true;
         }
         public Enemy(int health)
         {
             SetHealth(health);
+            IsAlive = true;
         }
         public Enemy(string name)
         {
             SetName(name);
+            IsAlive = true;
         }
-        public Enemy() { }
-        public int Health { get; private set; }
-        public string Name { get; private set; }
+        public Enemy() 
+        {
+            IsAlive = true;
+        }
+        public int SetHealth(int newHealth)
+        {
+            if (int.IsPositive(newHealth) && newHealth <= 100)
+                Health = newHealth;
+            return Health;
+        }
+        public string SetName(string newName)
+        {
+            if (!string.IsNullOrWhiteSpace(newName))
+                Name = newName;
+            return Name;
+        }
+        // TODO: aggiungere possibilità di morte e robe varie boh non mi ricordo.
         public void TakeDamage (int damage)
         {
             if (int.IsPositive(damage) && Health - damage > 0)
